d70548e5ca08c5700ff01fe2f63e37e1486d9f67
Maarten Paauw <maartenpaauw@gmail.com>
2015-07-12 11:47:06 +0200

    Updated the config array

--- src/config/yo.php
+++ src/config/yo.php
@@ -6,9 +6,21 @@ return [
     | Yo API key
     |--------------------------------------------------------------------------
     |
-    | The Yo API key. Can be found at the profile page via this link:
+    | The default Yo API key. Can be found at the profile page via this link:
     | https://dev.justyo.co/
     |
     */
-    'api_token' => ''
+    'api_token' => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Yo accounts
+    |--------------------------------------------------------------------------
+    |
+    | An array of the Yo API accounts with as key the account name (uppercase)
+    | and as value the Yo API key. The key can be found at the profile page
+    | via this link: https://dev.justyo.co/
+    |
+    */
+    'accounts' => []
 ];
